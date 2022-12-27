5cff76784a08df7174245f24f71133f3ba897ad1
Jayden Jones <jones.jaydenm@gmail.com>
2022-12-26 19:57:39 -0500

    Added the functions to change the selected area to 'O' or 'X' depending on the current turn.

--- java.js
+++ java.js
@@ -1,6 +1,22 @@
 const topLeft = document.querySelector('.topleft');
+const topCenter = document.querySelector('.topcenter');
+const topRight = document.querySelector('.topright');
+const middleLeft = document.querySelector('.middleleft');
+const middleCenter = document.querySelector('.middlecenter');
+const middleRight = document.querySelector('.middleright');
+const bottomLeft = document.querySelector('.bottomleft');
+const bottomCenter = document.querySelector('.bottomcenter');
+const bottomRight = document.querySelector('.bottomright');
 
-topLeft.onclick = () => fillBox();
+topLeft.onclick = (e) => fillBox(e);
+topCenter.onclick = (e) => fillBox(e);
+topRight.onclick = (e) => fillBox(e);
+middleCenter.onclick = (e) => fillBox(e);
+middleRight.onclick = (e) => fillBox(e);
+middleLeft.onclick = (e) => fillBox(e);
+bottomLeft.onclick = (e) => fillBox(e);
+bottomCenter.onclick = (e) => fillBox(e);
+bottomRight.onclick = (e) => fillBox(e);
 
 //Default Variable Values
 currentTurn = 'X'
@@ -9,8 +25,36 @@ OWins = false;
 
 
 // Fills in the clicked on box with the current turns symbol
-function fillBox() {
-    topLeft.innerHTML = currentTurn;
+function fillBox(e) {
+    let selection = e.target.value;
+    if (selection == "topLeft") {
+        topLeft.innerHTML = currentTurn;
+    }
+    if (selection == "topCenter") {
+        topCenter.innerHTML = currentTurn;
+    }
+    if (selection == "topRight") {
+        topRight.innerHTML = currentTurn;
+    }
+    if (selection == "middleLeft") {
+        middleLeft.innerHTML = currentTurn;
+    }
+    if (selection == "middleCenter") {
+        middleCenter.innerHTML = currentTurn;
+    }
+    if (selection == "middleRight") {
+        middleRight.innerHTML = currentTurn;
+    }
+    if (selection == "bottomLeft") {
+        bottomLeft.innerHTML = currentTurn;
+    }
+    if (selection == "bottomCenter") {
+        bottomCenter.innerHTML = currentTurn;
+    }
+    if (selection == "bottomRight") {
+        bottomRight.innerHTML = currentTurn;
+    }
+    
     changeTurn();
 }
 
