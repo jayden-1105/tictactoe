34e2539f3d0fcaf8c20788bed73a4a17971e99c4
Jayden Jones <jones.jaydenm@gmail.com>
2022-12-26 20:24:10 -0500

    Added in a variable for each section of the tictactoe board that gets designated when clicked as either X or O. Will use this for the logic to check for winners

--- java.js
+++ java.js
@@ -23,7 +23,15 @@ bottomRight.onclick = (e) => fillBox(e);
 currentTurn = 'X'
 XWins = false;
 OWins = false;
-
+r1c1 = "";
+r1c2 = "";
+r1c3 = "";
+r2c1 = "";
+r2c2 = "";
+r2c3 = "";
+r3c1 = "";
+r3c2 = "";
+r3c3 = "";
 
 // Fills in the clicked on box with the current turns symbol and disables the button
 function fillBox(e) {
@@ -31,39 +39,57 @@ function fillBox(e) {
     if (selection == "topLeft") {
         topLeft.innerHTML = currentTurn;
         topLeft.disabled = true;
+        r1c1 = currentTurn;
     }
     if (selection == "topCenter") {
         topCenter.innerHTML = currentTurn;
         topCenter.disabled = true;
+        r1c2 = currentTurn;
     }
     if (selection == "topRight") {
         topRight.innerHTML = currentTurn;
         topRight.disabled = true;
+        r1c3 = currentTurn;
     }
     if (selection == "middleLeft") {
         middleLeft.innerHTML = currentTurn;
         middleLeft.disabled = true;
+        r2c1 = currentTurn;
     }
     if (selection == "middleCenter") {
         middleCenter.innerHTML = currentTurn;
         middleCenter.disabled = true;
+        r2c2 = currentTurn;
     }
     if (selection == "middleRight") {
         middleRight.innerHTML = currentTurn;
         middleRight.disabled = true;
+        r2c3 = currentTurn;
     }
     if (selection == "bottomLeft") {
         bottomLeft.innerHTML = currentTurn;
         bottomLeft.disabled = true;
+        r3c1 = currentTurn;
     }
     if (selection == "bottomCenter") {
         bottomCenter.innerHTML = currentTurn;
         bottomCenter.disabled = true;
+        r3c2 = currentTurn;
     }
     if (selection == "bottomRight") {
         bottomRight.innerHTML = currentTurn;
         bottomRight.disabled = true;
+        r3c3 = currentTurn;
     }
+    console.log(r1c1);
+    console.log(r1c2);
+    console.log(r1c3);
+    console.log(r2c1);
+    console.log(r2c2);
+    console.log(r2c3);
+    console.log(r3c1);
+    console.log(r3c2);
+    console.log(r3c3);
     changeTurn();
     changeDisplay();
 }
